44edc024d1a449b5b3e54669dc0caa95138294e8
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-07-19 18:12:59 +0200

    good start

---
+++ Cloudremover/src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.model.Picture;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, JCodecException {
+
+        File videoFile = new File("/home/anton/Videos/blur.mp4");
+        Picture frame = FrameGrab.getFrameFromFile(videoFile, 0);
+        System.out.println("w: "+frame.getWidth());
+
+        
+    }
+}
